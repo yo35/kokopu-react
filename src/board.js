9f2b3d0a7346748df29c5a4e70ea8e2fe5879f72
Yoann Le Montagner <yo35@melix.net>
2021-02-09 20:21:32 +0100

    Render row/col coordinates

--- src/board.js
+++ src/board.js
@@ -7,9 +7,14 @@ export default class extends React.Component {
 
 	render() {
 		let rows = [...'87654321'].map(r => this.renderRow(r));
+		let columnCoordinates = [...'abcdefgh'].map(c => this.renderColumnCoordinate(c));
 		return (
 			<div className="kokopu-board kokopu-size42">
 				{rows}
+				<div className="kokopu-boardRow kokopu-columnCoordinates">
+					<div className="kokopu-boardCell"></div>
+					{columnCoordinates}
+				</div>
 			</div>
 		);
 	}
@@ -18,6 +23,7 @@ export default class extends React.Component {
 		let squares = [...'abcdefgh'].map(c => this.renderSquare(r, c));
 		return (
 			<div key={r} className="kokopu-boardRow">
+				<div className="kokopu-boardCell kokopu-rowCoordinate">{r}</div>
 				{squares}
 			</div>
 		);
@@ -26,6 +32,10 @@ export default class extends React.Component {
 	renderSquare(r, c) {
 		let sq = c + r;
 		return <Square key={c} id={sq} />;
+	}
+
+	renderColumnCoordinate(c) {
+		return <div key={c} className="kokopu-boardCell kokopu-columnCoordinate">{c}</div>;
 	}
 
 }
--- src/square.js
+++ src/square.js
@@ -5,7 +5,7 @@ import kokopu from 'kokopu';
 export default function(props) {
 	let colorClassName = kokopu.squareColor(props.id) === 'w' ? 'kokopu-lightSquare' : 'kokopu-darkSquare';
 	return (
-		<div className={['kokopu-square', 'kokopu-sized', colorClassName].join(' ')}>
+		<div className={['kokopu-boardCell', 'kokopu-square', 'kokopu-sized', colorClassName].join(' ')}>
 		</div>
 	);
 }
