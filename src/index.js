a6252fec8c893573cda73fcdb3ca8a108498b368
Yoann Le Montagner <yo35@melix.net>
2021-02-13 16:41:37 +0100

    Use Material UI for demo page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import kokopu from 'kokopu';
 import './chessboard.css';
 import Board from './board';
 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import Switch from '@material-ui/core/Switch';
+
 class App extends React.Component {
 
 	constructor(props) {
@@ -18,35 +24,44 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<button onClick={() => this.handlePositionClicked(new kokopu.Position('empty'))}>Empty position</button>
-				<button onClick={() => this.handlePositionClicked(new kokopu.Position())}>Start position</button>
-				<button onClick={() => this.handlePositionClicked('8/8/8/8/8/4k3/q7/4K3 b - - 0 1')}>Custom position</button>
-				<button onClick={() => this.handlePositionClicked('8/k1b/8/8/8/4k3/q7/4K3 b - - 0 1')}>Bad FEN</button>
-				<button onClick={() => this.handleFlipClicked()}>Flip</button>
-				<Board position={this.state.position} isFlipped={this.state.isFlipped}/>
-			</div>
+			<Grid container spacing={3}>
+				<Grid item xs={4}>
+					<p>
+						<ButtonGroup color="primary">
+							<Button onClick={() => this.handlePositionClicked(new kokopu.Position('empty'))}>Empty position</Button>
+							<Button onClick={() => this.handlePositionClicked(new kokopu.Position())}>Start position</Button>
+							<Button onClick={() => this.handlePositionClicked('8/8/8/8/8/4k3/q7/4K3 b - - 0 1')}>Custom position</Button>
+							<Button onClick={() => this.handlePositionClicked('8/k1b/8/8/8/4k3/q7/4K3 b - - 0 1')}>Bad FEN</Button>
+						</ButtonGroup>
+					</p>
+					<p>
+						<FormControlLabel
+							control={<Switch checked={this.state.isFlipped} onChange={() => this.handleFlipClicked()} color="primary" />}
+							label="Flip"
+						/>
+					</p>
+				</Grid>
+				<Grid item xs={8}>
+					<div>
+						<Board position={this.state.position} isFlipped={this.state.isFlipped} />
+					</div>
+				</Grid>
+			</Grid>
 		);
 	}
 
 	handlePositionClicked(newPosition) {
-		let newState = this.cloneState();
+		let newState = {...this.state};
 		newState.position = newPosition;
 		this.setState(newState);
 	}
 
 	handleFlipClicked() {
-		let newState = this.cloneState();
+		let newState = {...this.state};
 		newState.isFlipped = !this.state.isFlipped;
 		this.setState(newState);
 	}
 
-	cloneState() {
-		return {
-			position: this.state.position,
-			isFlipped: this.state.isFlipped,
-		};
-	}
 }
 
 const appRoot = document.createElement('div');
