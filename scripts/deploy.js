a942b6fe5ecf5acddc296f4af823c22bfd5433b4
Yoann Le Montagner <yo35@melix.net>
2022-08-03 12:33:22 +0200

    Adapt deploy script to support documentation versioning

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -66,7 +66,14 @@ promptPassword(`Pass for ${USER}@${HOST}: `, pass => {
 
 		// Upload the documentation.
 		console.log('Upload documentation...');
-		return client.uploadDir('dist/docs', `${ROOT_DIR}/docs`);
+		return client.uploadDir('dist/docs', `${ROOT_DIR}/docs/${version}`);
+
+	}).then(() => {
+
+		// Redirect /docs/current to the directory corresponding to latest documentation version.
+		console.log(`Redirect /docs/current to /docs/${version}...`);
+		let htaccess = Readable.from([ `Redirect "/docs/current" "/docs/${version}"` ]);
+		return client.put(htaccess, `${ROOT_DIR}/docs/.htaccess`, { mode: 0o644 });
 
 	}).then(() => console.log('Done.')).catch(() => console.log(err)).finally(() => client.end());
 });
