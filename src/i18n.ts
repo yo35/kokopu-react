0b328b2f92a34878429d3dc356aebb48af3ca36c
Yoann Le Montagner <yo35@melix.net>
2024-01-20 15:08:58 +0100

    Add homepage in copyright notice + package.json

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,14 +1,16 @@
-/* -------------------------------------------------------------------------- *
+/*!
+ * -------------------------------------------------------------------------- *
  *                                                                            *
- *    This file is part of Kokopu-React, a JavaScript chess library.          *
+ *    Kokopu-React - A React-based library of chess-related components.       *
+ *    <https://www.npmjs.com/package/kokopu-react>                            *
  *    Copyright (C) 2021-2024  Yoann Le Montagner <yo35 -at- melix.net>       *
  *                                                                            *
- *    This program is free software: you can redistribute it and/or           *
+ *    Kokopu-React is free software: you can redistribute it and/or           *
  *    modify it under the terms of the GNU Lesser General Public License      *
  *    as published by the Free Software Foundation, either version 3 of       *
  *    the License, or (at your option) any later version.                     *
  *                                                                            *
- *    This program is distributed in the hope that it will be useful,         *
+ *    Kokopu-React is distributed in the hope that it will be useful,         *
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            *
  *    GNU Lesser General Public License for more details.                     *
